7ab1c70f13d83984f6ea968bd376b4dc7c503c24
S0tab <William.s.t@live.dk>
2022-02-03 10:01:32 +0100

    n

--- RPG Game Projektværksted/Assets/questgiver.cs
+++ RPG Game Projektværksted/Assets/questgiver.cs
@@ -1,18 +1,16 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
 
 public class questgiver : MonoBehaviour
 {
-    // Start is called before the first frame update
-    void Start()
+    
+    private void OnCollisionEnter(Collision other)
     {
-        
-    }
-
-    // Update is called once per frame
-    void Update()
-    {
-        
+        if (other.gameObject.tag == "Player")
+        {
+           
+        }
     }
 }
